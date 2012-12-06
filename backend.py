4c1565be78455bcd66689237fd1143ad6563825f
Scott Ferguson <scott.ferguson@vokalinteractive.com>
2012-12-06 11:15:45 -0600

    Check facebook login first

--- backend.py
+++ backend.py
@@ -5,8 +5,6 @@ from passlib.hash import sha256_crypt
 import logging
 
 class GaeAuthBackend:
-    # Create an authentication method
-    # This is called by the standard Django login procedure
     def authenticate(self, username=None, password=None):
         # Try to find a user matching your username
         q = User.all()
@@ -16,20 +14,15 @@ class GaeAuthBackend:
         if not user:
             return None
 
-        #  Check the password is the reverse of the username
-        if user.password:
-            logging.info("Verifying password") 
-            if sha256_crypt.verify(password, user.password):
-                # Yes? return the Django user object
+        for t in user.facebooktoken_set:
+            logging.info(t.token)
+            if sha256_crypt.verify(password, t.token):
                 return user
-        else:
-            logging.info("Verifying token") 
-            for t in user.facebooktoken_set:
-                logging.info(t.token)
-                if sha256_crypt.verify(password, t.token):
-                    return user
 
-        # No? return None - triggers default login failed
+        if user.password:
+            if sha256_crypt.verify(password, user.password):
+                return user
+
         return None
 
     # Required for your backend to work properly - unchanged in most scenarios
